0dacd2d33f56f0bf9e2a6a14e341ff0088a5301f
Tutor Exilius <tutorexilius@gmail.com>
2018-10-13 16:50:51 +0200

    Added Version Info and Header Informations. Added Resource File.

---
+++ CSVParser/CSVParser/app_info.h
@@ -0,0 +1,17 @@
+// App Informations here
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
+#define VERSION_PATCH 0
+
+#define APP_NAME "CSV Parser"
+#define AUTHOR "Tutor Exilius"
+#define AUTHOR_EMAIL "tutorexilius@gmail.com"
+#define COPYRIGHT "Tutor Exilius - Copyright 2018"
+
+#define STRING_VERSION "" STR(VERSION_MAJOR) "." STR(VERSION_MINOR) "." STR(VERSION_PATCH)
+#define PRODUCT_VERSION VERSION_MAJOR,VERSION_MINOR,VERSION_PATCH,0
+#define STRING_COPYRIGHT AUTHOR " (" AUTHOR_EMAIL ") - " COPYRIGHT
--- CSVParser/CSVParser/main.cpp
+++ CSVParser/CSVParser/main.cpp
@@ -1,3 +1,20 @@
+/*
+
+Author:			Tutor Exilius (http://www.exilius.de)
+
+Created:		12.10.2018
+Last update:	13.10.2018
+
+GNU General Public License v3.0
+
+Permissions of this strong copyleft license are conditioned on making available
+complete source code of licensed works and modifications, which include larger
+works using a licensed work, under the same license.Copyright and license notices
+must be preserved.Contributors provide an express grant of patent rights.
+
+*/
+
+
 #include <iostream>
 #include <string>
 
